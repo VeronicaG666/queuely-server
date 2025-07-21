ceac8ab4a256551077ca92eeb30f26d13beb1bc2
VeronicaG666 <varnikaigdtuw@gmail.com>
2025-07-21 20:27:13 +0530

    One more.

--- db.js
+++ db.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false, // set to { rejectUnauthorized: false } if using a remote DB
+  ssl: {
+    rejectUnauthorized: false, // set to { rejectUnauthorized: false } if using a remote DB
+  },
 });
 
 module.exports = pool;
